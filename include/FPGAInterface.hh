248a8ab7fdf995b1fc0b4200f0f45fa56c3f6054
Sommema4 <sommema4@gmail.com>
2020-11-26 12:45:10 +0100

    Update FPGAInterface.hh
    10bit

--- include/FPGAInterface.hh
+++ include/FPGAInterface.hh
@@ -27,8 +27,8 @@
 
 #define OSCBASE         0x40100000
 #define OSCBASESIZE     0x30000
-#define OSCCHAOFFSET    0x10000
-#define OSCCHBOFFSET    0x20000
+#define OSCCHAOFFSET    0x400
+#define OSCCHBOFFSET    0x800
 
 #define ADCBITS 14
 
